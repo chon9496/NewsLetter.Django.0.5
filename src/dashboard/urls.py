8ad10bf263af3d005f665ac3191fdf54d533fd93
chon9496 <87382634+chon9496@users.noreply.github.com>
2022-05-16 01:14:06 -0500

    create y list

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import DashboardHomeView,NewslettersDashboardHomeView
+from .views import DashboardHomeView,NewslettersDashboardHomeView,NewsletterCreateView
 
 app_name="dashboard"
 
 urlpatterns = [
     path('',DashboardHomeView.as_view(),name="home"),
-    path('list/',NewslettersDashboardHomeView.as_view(),name="list")
+    path('list/',NewslettersDashboardHomeView.as_view(),name="list"),
+    path('create/',NewsletterCreateView.as_view(),name="create"),
+    
 ]
