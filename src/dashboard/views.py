dd02620e75e3363e668a61dbb5aafef51c15afa0
chon9496 <87382634+chon9496@users.noreply.github.com>
2022-05-16 01:46:16 -0500

    lista

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.views.generic import TemplateView, View
 from newsletters.models import Newsletter
 
 from newsletters.forms import NewsletterCreationForm
+
+from django.conf import settings
+from django.core.mail import send_mail, EmailMultiAlternatives, EmailMessage 
+
 
 
 # Create your views here.
@@ -29,6 +33,19 @@ class NewsletterCreateView(View):
         return render (request,'dashboard/create.html',context)
     
     def post (self, request, *args, **kwargs):
+        form = NewsletterCreationForm(request.POST or None)
+        
+        if form.is_valid():
+            instance=form.save()
+            newsletter=Newsletter.objects.get(id=instance.id)
+            
+            if newsletter.status=="published":
+                subject=newsletter.subject
+                body = newsletter.body
+                from_email = settings.EMAIL_HOST_USER
+                for email in newsletter.email.all():
+                    send_mail(subject=subject, from_email=from_email, recipient_list=[email],message=body,fail_silently=True)
+            return redirect('dashboard:list')
         form=NewsletterCreationForm()
         context={
             'form':form
