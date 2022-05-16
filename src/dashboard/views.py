fccf8d1d5b8d594736177cfb12014c65d28260e8
chon9496 <87382634+chon9496@users.noreply.github.com>
2022-05-16 01:14:18 -0500

    NewsletterCreationForm

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from newsletters.models import Newsletter
 
+from newsletters.forms import NewsletterCreationForm
+
+
 # Create your views here.
 class DashboardHomeView(TemplateView):
     template_name="dashboard/index.html"
@@ -16,3 +19,18 @@ class NewslettersDashboardHomeView(View):
             'newsletters':newsletters
         }
         return render(request, 'dashboard/list.html', context)
+    
+class NewsletterCreateView(View):
+    def get (self, request, *args, **kwargs):
+        form=NewsletterCreationForm()
+        context={
+            'form':form
+        }
+        return render (request,'dashboard/create.html',context)
+    
+    def post (self, request, *args, **kwargs):
+        form=NewsletterCreationForm()
+        context={
+            'form':form
+        }
+        return render (request,'dashboard/create.html',context)
